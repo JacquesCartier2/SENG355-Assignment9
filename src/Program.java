899523f2ecff4b39571e05375f6b12bed789c7fb
JacquesCartier2 <20jackson03@gmail.com>
2025-01-31 15:47:02 -0500

    deleted program code

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 
 public class Program {
 
 	public static void main(String[] args) {
-		KnapsackObject testOb = new Stock(1,1,1);
-		System.out.println(testOb.GetCost());
+		
 	}
 
 }
